f52cc6c8c76ca5ede83b78f68723274562815816
richie312 <richie.chatterjee31@gmail.com>
2020-08-19 20:17:15 +0530

    added the template and created the database and added the maoin.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import flask
 from flask import Flask, request, json,render_template,redirect,url_for,jsonify,json
 from dotenv import load_dotenv
+import mysql
+import mysql.connector
 import os
 import sys
 from decrypt import *
@@ -27,20 +29,39 @@ db_auth = {}
 for i in filename.keys():
     db_auth[i] = decrypt(eval(filename[i]),eval(file_key['key_'+i])).decode("utf-8")
 
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+# load the environement variables
+
+load_dotenv('.env')
+
+""" read the list of users"""
+@app.route("/")        
+def homepage():
+    return render_template("user_form.html")
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+@app.route('/addDetails', methods=['POST'])
+def addDetails():
+    connection = mysql.connector.connect(host=db_auth['host'], 
+                                         user=db_auth['dbuser'],
+                                         port=3306,
+                                         passwd=db_auth['db_pass'], 
+                                         db=db_auth['dbname'])
+    cursor = connection.cursor()
+    data = request.form
+    
+    
+    
+    
+    
+    # close the cursor and the connection
+    cursor.close()
+    connection.close()
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0',debug=True,port=5002)
 
 
 
