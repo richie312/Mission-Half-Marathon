47ec505208b123b0fae4adc73b56e84348010818
richie312 <richie.chatterjee31@gmail.com>
2020-08-20 15:39:03 +0530

    added the database connection and created the target table

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ import mysql
 import mysql.connector
 import os
 import sys
-from decrypt import *
+import pandas as pd
+from datetime import datetime
 
 """ decrypt the database details"""
 main_dir = os.getcwd()
 os.listdir(os.path.join(main_dir,'auth'))
+from decrypt import *
 
 db_auth = {'dbname.txt':'key_dbname.txt',
            'db_pass.txt':'key_db_pass.txt',
@@ -50,16 +52,38 @@ def addDetails():
                                          port=3306,
                                          passwd=db_auth['db_pass'], 
                                          db=db_auth['dbname'])
+    
     cursor = connection.cursor()
     data = request.form
     
+    start_time = datetime.strptime(data['StartTime'],'%H:%M')
+    end_time = datetime.strptime(data['EndTime'],'%H:%M')
     
+    # Convert total seconds into days, hours, minutes and, seconds.
+    duration = end_time - start_time
+    time_delta = pd.to_timedelta(duration.total_seconds(), unit='s')
+    #days = time_delta.components.days
+    hour = time_delta.components.hours
+    minutes = time_delta.components.minutes
+    secs = time_delta.components.seconds                
+    temp_duration = ("{}:{}:{}".format(hour,minutes,secs))
+    refined_duration = datetime.strptime(temp_duration,'%H:%M:%S')
     
+    query = """ INSERT INTO mission_half_marathon (Duration,DistanceCovered, 
+    OxygenLevel, PulseRate, Day,StartTime,EndTime)
+    VALUES (%s, %s, %s,%s,%s,%s,%s)"""
     
+    cursor.execute(query,(refined_duration,data['Distance'],data['Oxygen'],
+                              data['PulseRate'],data['Day'],
+                              data['StartTime'],data['EndTime']))
+    connection.commit()
     
-    # close the cursor and the connection
+    # close the cursor and connection
     cursor.close()
-    connection.close()
+    connection.close()    
+    
+    return render_template('user_form_response.html')
+    
 if __name__ == '__main__':
     app.run(host = '0.0.0.0',debug=True,port=5002)
 
